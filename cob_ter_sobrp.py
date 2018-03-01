0e2805886846ccc5c54f40c8a31215040ffc5f3c
thiagooarruda <32098436+thiagooarruda@users.noreply.github.com>
2018-03-01 11:45:09 -0300

    Update cob_ter_sobrp.py

--- cob_ter_sobrp.py
+++ cob_ter_sobrp.py
@@ -53,7 +53,7 @@ CT = [  'hid_massa_dagua_a',
             'veg_veg_cultivada_a']
 
 # Pegando o SRC da camada trecho de drenagem
-layerList = QgsMapLayerRegistry.instance().mapLayersByName('hid_trecho_drenagem_l')
+layerList = QgsMapLayerRegistry.instance().mapLayersByName('aux_moldura_a')
 if layerList:
  layer = layerList[0]
  SRC = layer.crs()
